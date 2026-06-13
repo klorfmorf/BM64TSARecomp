0dd53afa2b9c12b006e9ba29365d66a31da88269
RevoSucks <projectrevotpp@hotmail.com>
2026-06-12 20:09:07 -0400

    new launcher UI. v0.2.0

--- include/banjo_launcher.h
+++ include/banjo_launcher.h
@@ -3,13 +3,15 @@
 
 #include "recompui/recompui.h"
 
+#define RIGHT_POSITION_START 60.0f
+
 namespace banjo {
     void launcher_animation_setup(recompui::LauncherMenu *menu);
     void launcher_animation_update(recompui::LauncherMenu *menu);
 
-    constexpr float launcher_options_right_position_start = 96.0f;
-    constexpr float launcher_options_right_position_end = 96.0f + 24.0f;
-    constexpr float launcher_options_top_offset = 96.0f;
+    constexpr float launcher_options_right_position_start = RIGHT_POSITION_START;
+    constexpr float launcher_options_right_position_end = 0.0f + 24.0f;
+    constexpr float launcher_options_top_offset = -10.0f;
     constexpr float launcher_options_title_offset = 120.0f;
 }
 
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -64,7 +64,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.1.1";
+const std::string version_string = "0.2.0";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
@@ -707,7 +707,7 @@ int main(int argc, char** argv) {
 
     // Register fonts.
     recompui::register_primary_font("InterVariable.ttf", "Inter Variable");
-    recompui::register_extra_font("Suplexmentary Comic NC.ttf");
+    recompui::register_extra_font("Arial Rounded MT Bold Regular.ttf");
 
     // Register configuration path.
     recomp::register_config_path(recompui::file::get_app_folder_path());
