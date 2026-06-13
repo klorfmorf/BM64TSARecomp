bae3a0e949fb77121d4300f1f22cfadc961ee8e3
RevoSucks <projectrevotpp@hotmail.com>
2026-06-13 15:49:47 -0400

    remove old TLB workaround leftover. v0.2.3

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -64,7 +64,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.2.2";
+const std::string version_string = "0.2.3";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
